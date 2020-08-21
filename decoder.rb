bdb3e8c74de585562869243d4c91d78a9c711087
sorav <sauravitss24@gmail.com>
2020-08-21 17:33:15 +0530

    add h conversion1

---
+++ decoder.rb
@@ -0,0 +1,22 @@
+KEY = {
+    1 => 'A',
+    2 => 'B',
+    3 => 'C',
+    4 => 'D',
+    5 => 'E',
+    6=>  'H',
+}
+
+# Define a method that takes an array of numbers to decode.
+def decode(numbers)
+  text = ""
+  # Process each number in the array.
+  numbers.each do |number|
+    # Get the letter that corresponds to this number.
+    letter = KEY[number]
+    # Add it onto the string.
+    text += letter
+  end
+  # Return the decoded string.
+  return text
+end
