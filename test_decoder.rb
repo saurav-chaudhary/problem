3238d916f96490833386ab25a93d7fea0c7e5dfd
sorav <sauravitss24@gmail.com>
2020-08-21 17:47:26 +0530

    add both

---
+++ test_decoder.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './decoder'
+
+class TestDecode < Minitest::Test
+  def test_dad
+    assert_equal("DAD", decode([4, 1, 4]))
+    def test_bead
+        assert_equal("BEAD", decode([2, 5, 1, 4]))
+      end
+  end
+end
